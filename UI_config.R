a2754702efc534faea4bf8ed485842d4cc6a4d26
Chris Miller <christopher.ryan.miller@gmail.com>
2021-11-19 16:52:34 -0700

    Add barebones config file to be imported into the app.

---
+++ UI_config.R
@@ -0,0 +1,11 @@
+## Data input config ####
+
+# TODO: Make geographic data frame so a user can select state -> county
+county_list <- c("Sacramento", "San Francisco", "Denver")
+state_list <- c("CA", "CO")
+geography_granularity <- c("tract")
+
+# Data availability year
+# TODO: Error-handling around ACS vs Census
+year_list <- c(2018)
+
