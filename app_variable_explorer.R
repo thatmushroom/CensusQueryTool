13a5fce3e586b263af7ddb9d54915cd5c0d121e5
Chris Miller <christopher.ryan.miller@gmail.com>
2021-11-20 09:09:47 -0700

    Fetches data on a button press, rather than a reactive twitchy-fetch.

--- app_variable_explorer.R
+++ app_variable_explorer.R
@@ -34,26 +34,29 @@ ui <- fluidPage(# Application title
       selectInput(
         "input_year",
         h3("Select data year"),
-        choices = unique(acs_valid_options$year)
+        choices = sort(unique(acs_valid_options$year))
       ),
-      
-      
+      actionButton("go","Fetch Data")
     ),
     
-    # Show a plot of the generated distribution
+    # Show the table results
     mainPanel(dataTableOutput("available_variables"))
   ))
 
-# Define server logic required to draw a histogram
+# Define server logic required to pull data
 server <- function(input, output) {
   # Fetch raw data ####
-  output$available_variables <- renderDataTable({
+  
+  
+  data_return <- eventReactive(input$go,{
     available_variables <- load_variables(
       year = input$input_year,
       dataset = input$input_dataset,
       cache = TRUE
     )
   })
+  
+  output$available_variables <- renderDataTable(data_return())
   
 }
 
