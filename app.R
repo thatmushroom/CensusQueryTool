2d277b3a7f4ab0c786f9b0906fb52d4ce313b642
Chris Miller <christopher.ryan.miller@gmail.com>
2021-12-01 16:57:20 -0700

    https://github.com/thatmushroom/CensusQueryTool/issues/1

--- app.R
+++ app.R
@@ -19,16 +19,9 @@ source('./UI_config.R')
 ui <- fluidPage(# Application title
   titlePanel("Old Faithful Geyser Data"),
   
-  # Sidebar with a slider input for number of bins
+  # Sidebar with dropdowns for different possible ACS+Census choices
   sidebarLayout(
     sidebarPanel(
-      sliderInput(
-        "bins",
-        "Number of bins:",
-        min = 1,
-        max = 50,
-        value = 30
-      ),
       # Insert geography
       selectInput(
         "input_state",
@@ -42,6 +35,7 @@ ui <- fluidPage(# Application title
         choices = county_list,
         selected = county_list[1]
       ),
+      # Insert year
       selectInput(
         "input_year",
         h3("Select data year"),
@@ -51,26 +45,15 @@ ui <- fluidPage(# Application title
     ),
     
     # Show a plot of the generated distribution
-    # mainPanel(plotOutput("distPlot"))
-    mainPanel(tableOutput("cars"))
+    mainPanel(plotOutput("cars"))
   ))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x,
-         breaks = bins,
-         col = 'darkgray',
-         border = 'white')
-  })
-  
   # Fetch raw data ####
-  output$cars <- renderTable({
+  output$cars <- renderPlot({
+    # TODO: Break into pull_data(), transform_data(),  join_data()?
+    #
     cars_df <- get_acs(
       state = input$input_state,
       county = input$input_county,
@@ -89,22 +72,94 @@ server <- function(input, output) {
         "B25044_014",
         "B25044_015"
       ),
-      cache_table = TRUE,
-      geometry = FALSE,
+      geometry = TRUE,
+      # cache = TRUE,
       year = strtoi(input$input_year)
     )
-    # Subset for testing sake
-    cars_df %<>% slice_head(n = 10)
     
-    
-    hh <- get_acs(
+    hh_df <- get_acs(
       state = input$input_state,
       county = input$input_county,
       geography = "tract",
       variables = "B11001_001",
+      # TODO: Look up + comment this ACS varaible
       year = strtoi(input$input_year)
     )
+    # End fetching of data
+    
+    # Begin data transformation
+    cars_df %<>%
+      rename(cars_estimate = estimate,
+             cars_moe = moe) %>%
+      mutate(
+        cars_total_estimate = case_when(
+          variable %in% c("B25044_003", "B25044_010") ~ 0 * cars_estimate,
+          variable %in% c("B25044_004", "B25044_011") ~ 1 * cars_estimate,
+          variable %in% c("B25044_005", "B25044_012") ~ 2 * cars_estimate,
+          variable %in% c("B25044_006", "B25044_013") ~ 3 * cars_estimate,
+          variable %in% c("B25044_007", "B25044_014") ~ 4 * cars_estimate,
+          variable %in% c("B25044_008", "B25044_015") ~ 5 * cars_estimate
+        )
+      ) %>%
+      mutate(
+        cars_total_moe = case_when(
+          variable %in% c("B25044_003", "B25044_010") ~ 0 * cars_moe,
+          variable %in% c("B25044_004", "B25044_011") ~ 1 * cars_moe,
+          variable %in% c("B25044_005", "B25044_012") ~ 2 * cars_moe,
+          variable %in% c("B25044_006", "B25044_013") ~ 3 * cars_moe,
+          variable %in% c("B25044_007", "B25044_014") ~ 4 * cars_moe,
+          variable %in% c("B25044_008", "B25044_015") ~ 5 * cars_moe
+        )
+      ) %>%
+      mutate(cars_total_se = cars_moe / 1.645,
+             cars_total_se_2 = cars_total_se ^ 2)
+    
+    
+    cars_by_geoid <- cars_df %>%
+      group_by(GEOID) %>%
+      summarise(
+        cars_total_estimate = sum(cars_total_estimate),
+        # cars_total_moe_sum = sum(cars_total_se),
+        cars_total_moe = 1.645 * sqrt(sum(cars_total_se_2)),
+        NAME = unique(NAME)
+      )
+    
+    hh_df %<>%
+      rename(hh_estimate = estimate,
+             hh_moe = moe)
+    # End data transformation
+    
+    # Begin geography - TODO -
+    geoid <- left_join(cars_df %>%
+                         select(-variable),
+                       hh_df %>%
+                         select(-c("NAME", "variable")),
+                       by = "GEOID")
+    
+    df <- left_join(cars_by_geoid,
+                    hh_df %>%
+                      select(-c("NAME", "variable")),
+                    by = "GEOID") %>%
+      filter(GEOID != "06075980401") %>%
+      mutate(cars_per_hh_estimate = cars_total_estimate / hh_estimate)
+    
+    
+    
+    # End geography
+    # Assign the dataframe to the output object so it can be used elsewhere
+    # Not entirely sure how to do within a reactive context
+    #output$car_df <- isolate(df)???
+    # TODO. May end up wrapping one function to create both output
+    
+    
+    df %>%
+      ggplot(aes(fill = cars_per_hh_estimate)) +
+      geom_sf(color = NA) +
+      coord_sf(crs = 26910) +
+      scale_fill_viridis_c(option = "magma")
+    
   })
+  
   
 }
 
